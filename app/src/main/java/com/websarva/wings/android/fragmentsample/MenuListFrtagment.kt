1d79b92375d9d601968b53612553377f84352c02
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-07-19 15:05:19 +0900

    アクティビティーにフラグメントを埋め込み

--- app/src/main/java/com/websarva/wings/android/fragmentsample/MenuListFrtagment.kt
+++ app/src/main/java/com/websarva/wings/android/fragmentsample/MenuListFrtagment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
+import android.widget.SimpleAdapter
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,24 +19,50 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class MenuListFrtagment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_menu_list,container,false)
+        val lvMenu = view.findViewById<ListView>(R.id.lvMenu)
+        val menuList:MutableList<MutableMap<String,String>> = mutableListOf()
+        var menu = mutableMapOf("name" to "から揚げ定食","price" to "800円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "ハンバーグ定食", "price" to "850円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "生姜焼き定食", "price" to "850円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "ステーキ定食", "price" to "1000円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "野菜炒め定食", "price" to "750円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "とんかつ定食", "price" to "900円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "ミンチかつ定食", "price" to "850円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "チキンカツ定食", "price" to "900円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "コロッケ定食", "price" to "850円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "回鍋肉定食", "price" to "750円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "麻婆豆腐定食", "price" to "800円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "青椒肉絲定食", "price" to "900円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "焼き魚定食", "price" to "850円")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "焼肉定食", "price" to "950円")
+        menuList.add(menu)
+
+        val from = arrayOf("name","price")
+        val to = intArrayOf(android.R.id.text1,android.R.id.text2)
+        val adapter =SimpleAdapter(activity,menuList,android.R.layout.simple_list_item_2,from,to)
+        lvMenu.adapter = adapter
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_list, container, false)
+        return view
     }
 
     companion object {
